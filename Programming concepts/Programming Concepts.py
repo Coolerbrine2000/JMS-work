409b742883fb2bda554de0da8f3a11b27ee58b67
Coolerbrine2000 <arielsibanda737@gmail.com>
2025-03-19 12:47:31 +0200

    first

---
+++ Programming concepts/Programming Concepts.py
@@ -0,0 +1,22 @@
+#                                    Sequences
+
+Number1 = int(input("Enter your first number: "))
+Number2 = int(input("Enter your second number: "))
+Total_sum = Number1+Number2
+
+print("your first number is " +str(Number1))
+print("your second number is " +str(Number2))
+
+print("The total sum of the two numbers is: " +str(Total_sum))
+
+#                                     Selection
+
+a = 200
+b = 33
+
+if b>a:
+    print(" b is greater than a")
+elif a==b:
+    print("a and b are equal")
+else:
+    print("a is greater than b")
